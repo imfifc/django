83a7bbd2c297136a7972db6e13e8947964da863d
p_pjmzhu <p_pjmzhu@tencent.com>
2022-01-18 02:14:04 +0800

    path re_path

--- index/urls.py
+++ index/urls.py
@@ -1,6 +1,17 @@
-from django.urls import path
+from django.urls import path, re_path
 from index import views
 
+"""
+str，匹配除了路径分隔符（/）之外的非空字符串，这是默认的形式；
+int，匹配正整数，包含0；
+slug，匹配字母、数字以及横杠、下划线组成的字符串；
+uuid，匹配格式化的 uuid，如 075194d3-6885-417e-a8a8-6c931e272f00；
+path，匹配任何非空字符串，包含了路径分隔符。
+
+第一是可以将捕获到的字符值转换为对应的类型；
+第二是对 URL 中传值的一种限制，避免视图处理出错
+(?P<name>pattern) 
+"""
 app_name = 'index'  # 注册 写在开始位置即可
 
 urlpatterns = [
@@ -10,4 +21,7 @@ urlpatterns = [
     path('base/', views.base_html),
     path('redict/', views.redict_url),
     path('reverse/', views.test_to_reverse),
+    # re_path('test/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<slug>[^/]+)/', views.article_test),
+    # re_path(r'^test/(?P<year>[0-9]{4})/$', views.year_test),
+    # path('test/<int:year>/', views.year_test),
 ]
--- 账单.py
+++ 账单.py
@@ -8,7 +8,7 @@ with open(r"C:\Users\Administrator\Desktop\新建文本文档.csv", "r+", encodi
         ll = i.split(' ')
         print(ll)
         rows.append(ll)
-with open(r"C:\Users\Administrator\Desktop\5月.csv", "w+", newline='', encoding='utf-8') as f:
+with open(r"1.csv", "w+", newline='', encoding='utf-8') as f:
     header = ['交易日期', '入帐日期', '交易描述', '交易金额', '交易货币', '入账金额', '入账货币']
     writer = csv.writer(f)
     writer.writerow(header)
